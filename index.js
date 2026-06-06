aaebe8b0ed4467954ccb57716291e53519c26f74
bbb153487-web <bbb153487@gmail.com>
2026-06-06 20:44:19 +0800

    Update index.js

--- index.js
+++ index.js
@@ -76,14 +76,14 @@ function menuFlex() {
         contents: [
           { type: "text", text: "🌏 MO 自動翻譯", weight: "bold", size: "xl", align: "center" },
           { type: "button", action: { type: "message", label: "🤖 自動翻譯", text: "設定 自動" } },
-          { type: "button", action: { type: "message", label: "🇹🇭 中泰雙向", text: "設定 中泰" } },
-          { type: "button", action: { type: "message", label: "🇻🇳 中越雙向", text: "設定 中越" } },
-          { type: "button", action: { type: "message", label: "🇺🇸 中英雙向", text: "設定 中英" } },
-          { type: "button", action: { type: "message", label: "🇯🇵 中日雙向", text: "設定 中日" } },
-          { type: "button", action: { type: "message", label: "🇰🇷 中韓雙向", text: "設定 中韓" } },
-          { type: "button", action: { type: "message", label: "🇵🇭 中菲雙向", text: "設定 中菲" } },
-          { type: "button", action: { type: "message", label: "🇲🇲 中緬雙向", text: "設定 中緬" } },
-          { type: "button", action: { type: "message", label: "🇷🇺 中俄雙向", text: "設定 中俄" } },
+          { type: "button", action: { type: "message", label: "🇹🇭 中泰翻譯", text: "設定 中泰" } },
+          { type: "button", action: { type: "message", label: "🇻🇳 中越翻譯", text: "設定 中越" } },
+          { type: "button", action: { type: "message", label: "🇺🇸 中英翻譯", text: "設定 中英" } },
+          { type: "button", action: { type: "message", label: "🇯🇵 中日翻譯", text: "設定 中日" } },
+          { type: "button", action: { type: "message", label: "🇰🇷 中韓翻譯", text: "設定 中韓" } },
+          { type: "button", action: { type: "message", label: "🇵🇭 中菲翻譯", text: "設定 中菲" } },
+          { type: "button", action: { type: "message", label: "🇲🇲 中緬翻譯", text: "設定 中緬" } },
+          { type: "button", action: { type: "message", label: "🇷🇺 中俄翻譯", text: "設定 中俄" } },
           { type: "button", style: "primary", action: { type: "message", label: "🌍 多國翻譯", text: "設定 多國" } }
         ]
       }
