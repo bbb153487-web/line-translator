3979228a9a3b984c79028dd6c85e785a9f8537bb
bbb153487-web <bbb153487@gmail.com>
2026-06-13 00:48:52 +0800

    Update index.js

--- index.js
+++ index.js
@@ -421,11 +421,3 @@ app.get("/", (req, res) => {
 app.listen(process.env.PORT || 3000, () => {
   console.log("Server running");
 });
-
-app.get("/", (req, res) => {
-  res.send("LINE GPT Translator Bot Running");
-});
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Server running");
-});
