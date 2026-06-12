83806349d47dac900430adc5fe8f7fd732e15549
bbb153487-web <bbb153487@gmail.com>
2026-06-13 03:07:59 +0800

    Update index.js

--- index.js
+++ index.js
@@ -454,7 +454,7 @@ ${freeTranslated}`);
     await replyText(event, translated);
     return res.status(200).end();
 
-} catch (err) {
+    catch (err) {
     console.error(err);
     return res.status(200).end();
   }
